6f3031da391d0b4cbca10803ee1a8e2f494d7e88
kirillnsb <73110914+kirillnsb@users.noreply.github.com>
2020-10-31 20:57:31 +0300

    Add files via upload

---
+++ ArchiveComparator.java
@@ -0,0 +1,101 @@
+package kirillnsb.archivecomparator;
+
+import java.io.*;
+import java.util.List;
+import java.util.Objects;
+
+public class ArchiveComparator {
+
+    String name;
+    int hc;
+    long size;
+    boolean newFileFlag = true;
+    private String archive1Path;
+    private String archive2Path;
+    private List<ArchiveComparator> archive1Files = null;
+    private List<ArchiveComparator> archive2Files = null;
+
+    public ArchiveComparator() {
+    }
+
+    public ArchiveComparator(String archive1Path, String archive2Path) throws IOException {
+        this.archive1Path = archive1Path;
+        this.archive2Path = archive2Path;
+        setArchive1Files(FileWorker.scanArchive(archive1Path));
+        setArchive2Files(FileWorker.scanArchive(archive2Path));
+        FileWorker.writeStringToFile(compare(), "result.txt");
+    }
+
+    public void setArchive1Files(List<ArchiveComparator> archive1Files) {
+        this.archive1Files = archive1Files;
+    }
+
+    public void setArchive2Files(List<ArchiveComparator> archive2Files) {
+        this.archive2Files = archive2Files;
+    }
+
+    private String compare() throws IOException {
+        String res = "   " + FileWorker.getFileName(archive1Path) + "  |  " + FileWorker.getFileName(archive2Path) + "\n\n";
+        ArchiveComparator a1 = null;
+        ArchiveComparator a2 = null;
+        for (ArchiveComparator a1File : archive1Files) {
+            a1 = a1File;
+            boolean haveAnalog = false;
+            for (int i = 0; i < archive2Files.size(); i++) {
+                a2 = archive2Files.get(i);
+                if (a1.equals(a2)) {
+                    res += "= " + a1.name + "  |  " + "= " + a2.name + "\n";    //same case
+                    setNewFileFlagToFalse(i, a2);
+                    haveAnalog = true;
+                    break;
+                } else if (a1.name.equals(a2.name) && (a1.hc != a2.hc || a1.size != a2.size)) {
+                    res += "* " + a1.name + "  |  " + "* " + a2.name + "\n";    //updated case
+                    setNewFileFlagToFalse(i, a2);
+                    haveAnalog = true;
+                    break;
+                } else if (a1.size == a2.size) {
+                    res += "? " + a1.name + "  |  " + "? " + a2.name + "\n";    //renamed case
+                    setNewFileFlagToFalse(i, a2);
+                    haveAnalog = true;
+                    break;
+                }
+            }
+            if (!haveAnalog) res += "- " + a1.name + "  |\n";       //deleted case
+        }
+        for (ArchiveComparator a2File : archive2Files) {
+            a2 = a2File;
+            if (a2.newFileFlag)
+                res += "\t\t  |  " + "+ " + a2.name;                //new case
+        }
+
+        System.out.println(res);
+        return res;
+    }
+
+    private void setNewFileFlagToFalse(int index, ArchiveComparator a) {
+        a.newFileFlag = false;
+        archive2Files.set(index, a);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArchiveComparator that = (ArchiveComparator) o;
+        return hc == that.hc &&
+                size == that.size &&
+                Objects.equals(name, that.name);
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArchiveComparator ac = null;
+        try {
+            ac = new ArchiveComparator(args[0], args[1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            String archive1Path = FileWorker.openFileChooser("Select archive 1");
+            String archive2Path = FileWorker.openFileChooser("Select archive 2");
+            ac = new ArchiveComparator(archive1Path, archive2Path);
+        }
+
+    }
+}
---
+++ FileWorker.java
@@ -0,0 +1,62 @@
+package kirillnsb.archivecomparator;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class FileWorker {
+
+    public static String getFileName(String pathname) {
+        File file = new File(pathname);
+        return file.getName();
+    }
+
+    public static String openFileChooser(String dialogTitle) {
+        JButton open1 = new JButton();
+        JFileChooser fc = new JFileChooser();
+        fc.setCurrentDirectory(new File("."));
+        fc.setDialogTitle(dialogTitle);
+        FileNameExtensionFilter ef = new FileNameExtensionFilter("zip Files", "zip");
+        fc.setFileFilter(ef);
+        fc.showOpenDialog(open1);
+        return fc.getSelectedFile().getAbsolutePath();
+    }
+
+    public static void writeStringToFile(String str, String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        PrintWriter pw = new PrintWriter(file);
+        pw.print(str);
+        pw.close();
+        System.out.println("Result successfully saved in file: " + fileName);
+    }
+
+    /**
+     * Returns a List containing ArchiveComparator objects by scanning zip-archive.
+     *
+     * @param pathname path to archive
+     * @return a List containing ArchiveComparator objects by scanning zip-archive
+     * @throws IOException and as a case FileNotFoundException
+     */
+    public static List<ArchiveComparator> scanArchive(String pathname) throws IOException {
+        int index;
+        ZipEntry entry;
+        List<ArchiveComparator> archive1Files = new ArrayList<>();
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(pathname));
+        while ((entry = zis.getNextEntry()) != null) {
+            index = 0;
+            ArchiveComparator a1 = new ArchiveComparator();
+            a1.name = entry.getName();
+            a1.size = entry.getSize();
+            a1.hc = entry.hashCode();
+            archive1Files.add(index, a1);
+            zis.closeEntry();
+            index++;
+        }
+        zis.close();
+        return archive1Files;
+    }
+}
